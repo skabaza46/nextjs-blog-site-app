5e24b364530e943dae73b625162f8e9df0357ca7
skabaza <skabaza46@gmail.com>
2022-12-29 15:33:41 -0500

    added functionality for All Posts page.

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,11 +1,55 @@
+import AllPosts from "../../components/posts/all-posts";
+
 const { Fragment } = require("react")
+
+const DUMMY_POSTS = [
+  {
+    title:'Getting Started With NextJS', 
+    image:'getting-started-nextjs.png', 
+    excerpt:'NextJs is the React Framework for production - it makes building React apps and sites a breeze and ships with built-in Server Side Rendering ', 
+    date:'2022-02-10', 
+    slug:'getting-started-with-nextjs'
+  },
+  {
+    title:'Getting Started With NextJS2', 
+    image:'getting-started-nextjs.png', 
+    excerpt:'NextJs is the React Framework for production - it makes building React apps and sites a breeze and ships with built-in Server Side Rendering ', 
+    date:'2022-02-10', 
+    slug:'getting-started-with-nextjs2'
+  },
+  {
+    title:'Getting Started With NextJS3', 
+    image:'getting-started-nextjs.png', 
+    excerpt:'NextJs is the React Framework for production - it makes building React apps and sites a breeze and ships with built-in Server Side Rendering ', 
+    date:'2022-02-10', 
+    slug:'getting-started-with-nextjs3'
+  },
+  {
+    title:'Getting Started With NextJS4', 
+    image:'getting-started-nextjs.png', 
+    excerpt:'NextJs is the React Framework for production - it makes building React apps and sites a breeze and ships with built-in Server Side Rendering ', 
+    date:'2022-02-10', 
+    slug:'getting-started-with-nextjs4'
+  },
+  {
+    title:'Getting Started With NextJS5', 
+    image:'getting-started-nextjs.png', 
+    excerpt:'NextJs is the React Framework for production - it makes building React apps and sites a breeze and ships with built-in Server Side Rendering ', 
+    date:'2022-02-10', 
+    slug:'getting-started-with-nextjs5'
+  },
+  {
+    title:'Getting Started With NextJS6', 
+    image:'getting-started-nextjs.png', 
+    excerpt:'NextJs is the React Framework for production - it makes building React apps and sites a breeze and ships with built-in Server Side Rendering ', 
+    date:'2022-02-10', 
+    slug:'getting-started-with-nextjs6'
+  }
+];
 
 const AllPostsPage = () => {
 
-  return (
-    <Fragment>
-      <h1>All Posts Page</h1>
-    </Fragment>
+  return ( <AllPosts posts={DUMMY_POSTS}/>
   )
 };
 
