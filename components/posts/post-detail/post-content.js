cada13046cd0df5ae2d5f1337fb633e0dcd1b7dd
skabaza <skabaza46@gmail.com>
2022-12-29 21:27:54 -0500

    Rendering Images with the Nextjs Image componenet from Markdown.

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -1,19 +1,57 @@
-import PostHeader from "./post-header";
 import ReactMarkdown from 'react-markdown';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { atomDark } from 'react-syntax-highlighter/dist/cjs/styles/prism';
 
+import Image from 'next/image';
+import PostHeader from './post-header';
 import classes from './post-content.module.css';
 
-const PostContent = (props) => {
-    const { post } = props;
+function PostContent(props) {
+  const { post } = props;
 
-    const imagePath = `/images/posts/${post.slug}/${post.image}`;
+  const imagePath = `/images/posts/${post.slug}/${post.image}`;
 
-    return (
-        <article className={classes.content}>
-            <PostHeader title={post.title} image={imagePath} />
-            <ReactMarkdown>{post.content}</ReactMarkdown>
-        </article>
-    )
+  const customRenderers = {
+
+    paragraph(paragraph) {
+      const { node } = paragraph;
+
+      if (node.children[0].type === 'image') {
+        const image = node.children[0];
+
+        return (
+          <div className={classes.image}>
+            <Image
+              src={`/images/posts/${post.slug}/${image.url}`}
+              alt={image.alt}
+              width={600}
+              height={300}
+            />
+          </div>
+        );
+      }
+
+      return <p>{paragraph.children}</p>;
+    },
+
+    code(code) {
+      const { language, value } = code;
+      return (
+        <SyntaxHighlighter
+          style={atomDark}
+          language={language}
+          children={value}
+        />
+      );
+    },
+  };
+
+  return (
+    <article className={classes.content}>
+      <PostHeader title={post.title} image={imagePath} />
+      <ReactMarkdown renderers={customRenderers}>{post.content}</ReactMarkdown>
+    </article>
+  );
 }
 
 export default PostContent;
