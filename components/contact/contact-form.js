ee7f796c1e0000851123ae1a5db8b23f42d219ec
skabaza <skabaza46@gmail.com>
2022-12-29 23:12:59 -0500

    added UI Feedback with Notifications.

--- components/contact/contact-form.js
+++ components/contact/contact-form.js
@@ -1,26 +1,96 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import classes from './contact-form.module.css';
+import Notification from '../ui/notification';
 
+const sendContactData = async (contactDetails) => {
+    const response = await fetch('/api/contact', {
+        method: "POST",
+        body: JSON.stringify(contactDetails),
+        header: {
+            'Content-Type': "application/json"
+        }
+    });
+
+    const data = await response.json();
+
+    if(!response.ok){
+        throw new Error(data.message || "Something went wrong!")
+    }
+
+}
 const ContactForm = () => {
 
     const [ enteredEmail, setEnteredEmail ] = useState('');
     const [ enteredName, setEnteredName ] = useState('');
     const [ enteredMessage, setEnteredMessage ] = useState('');
 
-    const sendMessageHandler = (event) => {
+    const [ requestStatus, setRequestStatus ] = useState() // pending, success, error
+    const [ requestError, setRequestError ] =useState();
+
+    useEffect(()=>{
+        if(requestStatus === 'success' || requestStatus === 'error'){
+            const timer = setTimeout(()=>{
+                setRequestStatus(null);
+                setRequestError(null);
+
+            }, 3000);
+
+            return () => clearTimeout(timer);
+        }
+        
+
+    }, [requestStatus])
+
+    const sendMessageHandler = async (event) => {
         event.preventDefault();
 
-        fetch('/api/contact', {
-            method: "POST",
-            body: JSON.stringify({
+        setRequestStatus('pending')
+
+        try {
+            await sendContactData({
                 email: enteredEmail,
                 name: enteredEmail,
                 message: enteredMessage
-            }),
-            header: {
-                'Content-Type': "application/json"
-            }
-        });
+            });
+
+            setRequestStatus('success')
+
+            setEnteredMessage('');
+            setEnteredEmail('');
+            setEnteredName('');
+        } catch (error) {
+            setRequestError(error.message)
+            setRequestStatus('error')
+        }
+        
+    };
+
+    let notification;
+
+    if (requestStatus === 'pending'){
+        notification = {
+            status: 'pending',
+            title: "Sending message...",
+            message: "Your message is on it's way!"
+        };
+    };
+
+    if (requestStatus === 'success'){
+        notification = {
+            status: 'succes',
+            title: "Success",
+            message: "Message sent successfully!"
+        };
+    };
+
+
+    if (requestStatus === 'error'){
+        notification = {
+            status: 'error',
+            title: "Error",
+            message: requestError
+
+        };
     };
 
     return (
@@ -63,6 +133,7 @@ const ContactForm = () => {
                     <button>Send Message</button>
                 </div>
             </form>
+            {notification && <Notification {...notification}/>}
         </section>
     )
 }
--- next.config.js
+++ next.config.js
@@ -4,20 +4,20 @@ module.exports = (phase) => {
   if (phase === PHASE_DEVELOPMENT_SERVER) {
     return {
       env: {
-        mongodb_username: '',
-        mongodb_password: '',
-        mongodb_clustername: '',
-        mongodb_database: '',
+        mongodb_username: 'skabaza46',
+        mongodb_password: 'V2KAqQkepVrBgAhA',
+        mongodb_clustername: 'cluster0.xf2g1pw',
+        mongodb_database: 'nextjs-blog-site-dev',
       },
     };
   }
 
   return {
     env: {
-      mongodb_username: '',
-      mongodb_password: '',
-      mongodb_clustername: '',
-      mongodb_database: '',
+      mongodb_username: 'skabaza46',
+      mongodb_password: 'V2KAqQkepVrBgAhA',
+      mongodb_clustername: 'cluster0.xf2g1pw',
+      mongodb_database: 'nextjs-blog-site-prod',
     },
   };
 };
